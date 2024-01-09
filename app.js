f19acaf8d55b71f945c67a3507dd0d8417258c8c
Dani <117122272+MynoCommander@users.noreply.github.com>
2024-01-09 22:33:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class _app {
     backgroundToggler = false;
     shouldIgnoreVideo = false;
     effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-    brandDescription = ['Gfx Designer', 'Garxu Developer', 'Official Garxu'];
+    brandDescription = ['GFX OWNER', 'Shop Garxu', 'Official Garxu'];
 
     titleChanger = (text, delay) => {
         if (!text) return;
